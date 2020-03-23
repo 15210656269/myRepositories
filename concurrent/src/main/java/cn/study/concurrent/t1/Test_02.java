bc4c7e118e65008fdbadf15c4975ac26da7722a3
jiaqinwen <731971603@qq.com>
2020-03-23 23:49:35 +0800

    静态同步锁注释增加以及修改静态同步方法--->从synchronized static修改为static synchronized类型

--- concurrent/src/main/java/cn/study/concurrent/t1/Test_02.java
+++ concurrent/src/main/java/cn/study/concurrent/t1/Test_02.java
@@ -4,12 +4,14 @@ package cn.study.concurrent.t1;
  * synchronized关键字
  *
  * 同步 static
+ *
+ * 静态同步方法，锁的是当前类型的类对象，在本类中，就是Test_02.class对象
  */
 public class Test_02 {
 
     private static int staticCount = 0;
 
-    public synchronized static void testSync04(){
+    public static synchronized void testSync04(){
         System.out.println(Thread.currentThread().getName() + " staticCount = " + staticCount++);
     }
 
